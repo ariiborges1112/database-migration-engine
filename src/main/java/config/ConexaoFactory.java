831f5b909750ced49408bdc7573fe5014202e5a6
ariiborges1112 <ari.borges1112@gmail.com>
2026-08-04 03:54:05 -0300

    fix: realocação do  atributo urlBanco para fora do método

--- src/main/java/config/ConexaoFactory.java
+++ src/main/java/config/ConexaoFactory.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoFactory{
+    private static final String urlBanco = "jdbc:sqlite:Business-Template.db";
+
     public Connection conectar(){
         try{
-            String urlBanco = "jdbc:sqlite:Business-Template.db";
             Connection conexao = DriverManager.getConnection(urlBanco);
 
             System.out.println("Banco conectado com sucesso!");
