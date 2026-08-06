185caabb3d1aaed42c25712a10115a60eb83b74a
ariiborges1112 <ari.borges1112@gmail.com>
2026-08-06 03:45:36 -0300

    feat: adiciona metodo 'todasCategorias' que retorna todas as categorias do banco

--- src/main/java/dao/CategoriaDAO.java
+++ src/main/java/dao/CategoriaDAO.java
@@ -2,8 +2,10 @@ package dao;
 
 import model.Categoria;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoriaDAO{
@@ -14,7 +16,7 @@ public class CategoriaDAO{
     }
 
     public void insertCategoria(Categoria categoria){
-        String sql = "INSERT INTO categoria(nome) values (?)";
+        String sql = "INSERT INTO categoria (nome) values (?)";
 
         try(PreparedStatement stmt = conexao.prepareStatement(sql)){
             stmt.setString(1, categoria.getNome());
@@ -24,7 +26,23 @@ public class CategoriaDAO{
         }
     }
 
-    public List<Categoria> selectCategoria(Categoria categoria){
+    public List<Categoria> todasCategoria(){
+        String sql = "SELECT * FROM categoria";
 
+        try(PreparedStatement stmt = conexao.prepareStatement(sql)){
+            ResultSet rs = stmt.executeQuery();
+            List<Categoria> categoriaConsulta = new ArrayList<>();
+
+            while(rs.next()){
+                Integer id = rs.getInt("id");
+                String nome = rs.getString("nome");
+
+                Categoria consulta = new Categoria(id, nome);
+                categoriaConsulta.add(consulta);
+            }
+            return categoriaConsulta;
+        }catch(SQLException e){
+            throw new RuntimeException("Erro ao buscar todas as categoria", e);
+        }
     }
 }
