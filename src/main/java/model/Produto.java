e4c5830d03d4eae6ba83ed78eee45495e9e110c0
ariiborges1112 <ari.borges1112@gmail.com>
2026-08-06 00:38:10 -0300

    feat: criar classe modelo Produto

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -9,29 +9,85 @@ public class Produto{
     private BigDecimal precoVenda;
     private Integer quantidadeAtual;
     private Integer estoqueMinimo;
-    private Categoria categoriaId;
+    private Categoria categoria;
 
     public Produto(String nome, BigDecimal precoCusto, BigDecimal precoVenda,
-                   Integer estoqueMinimo, Categoria categoriaId){
-        this.nome = nome;
-        this.precoCusto = precoCusto;
-        this.precoVenda = precoVenda;
-        this.quantidadeAtual = 0;
-        this.estoqueMinimo = estoqueMinimo;
-        this.categoriaId = categoriaId;
+                   Integer estoqueMinimo, Categoria categoria){
+        this.setNome(nome);
+        this.setPrecoCusto(precoCusto);
+        this.setPrecoVenda(precoVenda);
+        this.setEstoqueMinimo(estoqueMinimo);
+        this.setCategoria(categoria);
+        this.setQuantidadeAtual(0);
     }
 
     public Produto(Integer id, String nome, BigDecimal precoCusto, BigDecimal precoVenda,
-                   Integer estoqueMinimo, Categoria categoriaId){
-        this(nome, precoCusto, precoVenda, estoqueMinimo, categoriaId);
-        this.id = id;
-        this.quantidadeAtual = 0;
+                   Integer quantidadeAtual, Integer estoqueMinimo, Categoria categoria){
+        this(nome, precoCusto, precoVenda, estoqueMinimo, categoria);
+        this.setId(id);
+        this.setQuantidadeAtual(quantidadeAtual);
     }
 
-    /*
-    CRIAR METODOS DE REGRA DE NEGOCIO
-    METODOS DE VALIDAÇÃO
-     */
+    private void validarId(Integer id){
+        if(id < 1) throw new IllegalArgumentException("O ID não pode ser menor que 1!");
+    }
+
+    private void validarNome(String nome){
+        if(nome == null || nome.trim().isEmpty()){
+            throw new IllegalArgumentException("O nome não pode ser vazio!");
+        }
+        if(!nome.matches("^[A-Za-zÀ-ÿ0-9\\s\\-\\.\\,%]+$")){
+            throw new IllegalArgumentException("Não é permitido caracteres especiais!");
+        }
+        if(nome.length() > 40){
+            throw new IllegalArgumentException("O nome não pode ter mais que 40 caracteres!");
+        }
+    }
+
+    private void validarPrecoCusto(BigDecimal precoCusto){
+        if(precoCusto == null){
+            throw new IllegalArgumentException("O valor de custo não pode ser nulo!");
+        }
+        if(precoCusto.compareTo(BigDecimal.ZERO) <= 0){
+            throw new IllegalArgumentException("O valor de custo não pode ser menor ou igual a 0!");
+        }
+    }
+
+    private void validarPrecoVenda(BigDecimal precoVenda){
+        if(precoVenda == null){
+            throw new IllegalArgumentException("O valor de venda não pode ser nulo!");
+        }
+        if(precoVenda.compareTo(BigDecimal.ZERO) < 0){
+            throw new IllegalArgumentException("O valor de venda não pode ser menor que 0!");
+        }
+    }
+
+    private void validarQuantidadeAtual(Integer quantidadeAtual){
+        if(quantidadeAtual == null){
+            throw new IllegalArgumentException("A quantidade atual não pode ser nula!");
+        }
+        if(quantidadeAtual < 0){
+            throw new IllegalArgumentException("A quantidade atual não pode ser menor que 0!");
+        }
+    }
+
+    private void validarEstoqueMinimo(Integer estoqueMinimo){
+        if(estoqueMinimo == null){
+            throw new IllegalArgumentException("O estoque minimo não pode ser nulo!");
+        }
+        if(estoqueMinimo < 0){
+            throw new IllegalArgumentException("O estoque minimo não pode ser menor que 0!");
+        }
+    }
+
+    private void validarCategoria(Categoria categoria){
+        if(categoria == null){
+            throw new IllegalArgumentException("A categoria não pode ser nula!");
+        }
+        if(categoria.getId() == null || categoria.getId() < 1){
+            throw new IllegalArgumentException("O ID deve existir e não pode ser menor ou igual a 0!");
+        }
+    }
 
     public Integer getId(){
         return id;
@@ -87,12 +143,12 @@ public class Produto{
         this.estoqueMinimo = estoqueMinimo;
     }
 
-    public Categoria getCategoriaId(){
-        return categoriaId;
+    public Categoria getCategoria(){
+        return categoria;
     }
 
-    public void setCategoriaId(Categoria categoriaId){
-        validarCategoriaId(categoriaId);
-        this.categoriaId = categoriaId;
+    public void setCategoria(Categoria categoria){
+        validarCategoria(categoria);
+        this.categoria = categoria;
     }
 }
